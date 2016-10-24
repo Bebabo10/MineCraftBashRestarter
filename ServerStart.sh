9d730d3ab47c91e3645b4db58215b0ce03899dba
Bebabo10 <Nylegarcia01@gmail.com>
2016-10-24 13:55:48 -0500

    Update ServerStart.sh

--- ServerStart.sh
+++ ServerStart.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+BASE="#!/bin/bash"
+RAM=12
+
 while true
 do
-java -server -Xms12G -Xmx12G -XX:PermSize=256m -XX:TargetSurvivorRatio=80 -XX:MaxTenuringThreshold=15 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:MaxGCPauseMillis=40 -XX:GCPauseIntervalMillis=150 -XX:+AggressiveOpts -XX:+TieredCompilation -XX:ReservedCodeCacheSize=2048m -XX:+UseCodeCacheFlushing -XX:CompileThreshold=1000 -Dfml.doNotBackup=true -jar forge.jar nogui
+
+
+java -server -Xms"$RAM"G -Xmx"$RAM"G -XX:PermSize=256m -XX:TargetSurvivorRatio=80 -XX:MaxTenuringThreshold=15 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:MaxGCPauseMillis=40 -XX:GCPauseIntervalMillis=150 -XX:+AggressiveOpts -XX:+TieredCompilation -XX:ReservedCodeCacheSize=2048m -XX:+UseCodeCacheFlushing -XX:CompileThreshold=1000 -Dfml.doNotBackup=true -jar $BASE nogui
 echo "If you want to completely stop the server process now, press Ctrl+C before
 the time is up!"
 echo "Rebooting in:"
