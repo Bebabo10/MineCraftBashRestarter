f935a76e7de2f8e36642087983fa6291bf539985
Bebabo10 <thebebabo10pack@gmail.com>
2016-10-25 22:00:44 -0500

    Added way to automaticly reboot server

--- ServerStart.sh
+++ ServerStart.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-BASE="#!/bin/bash"
+BASE="forge.jar"
 RAM=12
-
+session="hermitpack"
+args="-server -Xms12G -Xmx12G -XX:PermSize=256m -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dfml.doNotBackup=true --mods ./sponge.jar"
 while true
 do
 
+echo '/opt/restarter/restarter.sh $session' | at now + 5 hours
 
-java -server -Xms"$RAM"G -Xmx"$RAM"G -XX:PermSize=256m -XX:TargetSurvivorRatio=80 -XX:MaxTenuringThreshold=15 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:MaxGCPauseMillis=40 -XX:GCPauseIntervalMillis=150 -XX:+AggressiveOpts -XX:+TieredCompilation -XX:ReservedCodeCacheSize=2048m -XX:+UseCodeCacheFlushing -XX:CompileThreshold=1000 -Dfml.doNotBackup=true -jar $BASE nogui
+java $args -jar $BASE nogui
+
 echo "If you want to completely stop the server process now, press Ctrl+C before
 the time is up!"
 echo "Rebooting in:"
@@ -17,3 +20,4 @@ sleep 1
 done
 echo "Rebooting now!"
 done
+
