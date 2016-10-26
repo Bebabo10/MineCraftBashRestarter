83fdb674f346be88f03d9b0b02b909f872bf1e5c
Bebabo10 <Nylegarcia01@gmail.com>
2016-10-25 23:04:41 -0500

    fixed bugs

--- serverstart.sh
+++ serverstart.sh
@@ -2,17 +2,21 @@
 BASE="forge.jar"
 RAM=12
 session="hermitpack"
-args="-server -Xms"$RAM"G -Xmx"$RAM"G -XX:PermSize=256m -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dfml.doNotBackup=true --mods ./sponge.jar"
+args="-server -Xms"$RAM"G -Xmx"$RAM"G -XX:PermSize=256m -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dfml.doNotBackup=true"
 while true
 do
 
 echo '/opt/restarter/restarter.sh $session' | at now + 5 hours
 
-java $args -jar $BASE nogui
+java $args -jar $BASE nogui --mods ./sponge.jar
 
 echo "If you want to completely stop the server process now, press Ctrl+C before
 the time is up!"
 echo "Rebooting in:"
+
+#till i find a better way of doing this
+atrm {1..10000}
+
 for i in 5 4 3 2 1
 do
 echo "$i..."
