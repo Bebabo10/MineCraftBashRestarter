b93f0c21d0759f2f130230888a35cc109fef8ed8
Bebabo10 <Nylegarcia01@gmail.com>
2016-10-30 17:09:38 -0500

    Update commandget.sh

--- commandget.sh
+++ commandget.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 minecraftLog="/opt/hermitpack/logs/fml-server-latest.log"
+path="/opt/restarter"
 session=hermitpack 
  
 tail -fn0 $minecraftLog | \
@@ -14,33 +15,21 @@ min=$( echo "$line" | grep "ran the command: /sr*" | awk '{print $11}')
 sec=$( echo "$line" | grep "ran the command: /sr*" | awk '{print $12}')
  
 echo "$sname in $hour $min $sec"
-DR=0
- 
+
 cat /opt/hermitpack/ops.json | grep "name" | awk '{print $2}' | cut -d \" -f 2 | \
 while read name ; do
                
-if [ "$tester" = "$name" ]; then
+if [ "$sname" = "$name" ]; then
 echo "$name is on the list"
-               
-DR=$((DR+1))
-fi
-               
-done
- 
-if [ "$sname" = "Server" ]; then
-DR=$((DR+1))
-fi
- 
-if [ "$DR" -gt "0" ]; then
-               
 echo "ran"
-echo '/opt/restarter/restarter.sh $session $hour $min $sec'
-
-
-else
-echo "dint run"
- 
+sh $path/restarter.sh $session $hour $min $sec
+fi             
+done
+if [ "$sname" = "Server" ]; then
+echo "ran"
+sh $path/restarter.sh $session $hour $min $sec
 fi
+ 
      
 fi 
 done
