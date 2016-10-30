5ca40584a95340567b0961cca6cd96cbe1908469
Bebabo10 <Nylegarcia01@gmail.com>
2016-10-30 12:14:24 -0500

    cleanup

--- serverstart.sh
+++ serverstart.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 BASE="forge.jar"
 RAM=12
+hourstillreboot=5
 #do not have the same que for multiple servers
 #Use letters from c-z
 queue=h
-hourstillreboot=5
+
 
 session="hermitpack"
 args="-XX:PermSize=256m -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dfml.doNotBackup=true"
 while true
 do
-
 echo '/opt/restarter/restarter.sh $session' | at -q $queue now + "$hourstillreboot" hours
-
 java $args -server -Xms"$RAM"G -Xmx"$RAM"G -jar $BASE nogui --mods ./sponge.jar
+jid="$(atq -q $queue | awk '{print $1}')"
+atrm -q $queue $jid
 
 echo "If you want to completely stop the server process now, press Ctrl+C before
 the time is up!"
 echo "Rebooting in:"
-
-jid="$(atq -q $queue | awk '{print $1}')"
-
-atrm -q $queue $jid
 
 for i in 5 4 3 2 1
 do
